9602c93b85ab6f39a6e60d3fa19bb78b88ef9cbb
Zolouz966 <zrodrigo96@gmail.com>
2023-03-01 16:49:48 -0400

    Agregamos Styled Components a Formulario.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,22 +1,62 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+const Campo = styled.div `
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center;
+`;
+
+const Label = styled.div `
+    flex: 0 0 100px; 
+`;
+
+const Select = styled.select `
+    display: block;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #E1E1E1;
+    -webkit-appearance: none;
+`;
+
+const ImputRadio = styled.input `
+    margin: 0 1rem;
+
+`;
+
+const Boton = styled.button `
+    background-color: #2e922b;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    text-transform: uppercase;
+    color: #fff;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #7CC644;
+    }
+`;
 
 const Formulario = () => {
     return (
         <form>
-            <div>
-                <label for="">Marca</label>
-                <select>
+            <Campo>
+                <Label for="">Marca</Label>
+                <Select>
                     <option value="">Selecione</option>
                         <option value="americado">Americano</option>
                         <option value="europeo">Europeo</option>
                         <option value="asiatico">Asiático</option>
-                </select>
-            </div>
-            <div>
-                <label for="">Marca</label>
-                <select>
+                </Select>
+            </Campo>
+            <Campo>
+                <Label for="">Marca</Label>
+                <Select>
                     <option value="">-- Seleccione --</option>
                         <option value="2021">2021</option>
                         <option value="2020">2020</option>
@@ -28,23 +68,23 @@ const Formulario = () => {
                         <option value="2014">2014</option>
                         <option value="2013">2013</option>
                         <option value="2012">2012</option>
-                </select>
-            </div>
-            <div>
-                <label for="">Plan</label>
-                <input 
+                </Select>
+            </Campo>
+            <Campo>
+                <Label for="">Plan</Label>
+                <ImputRadio 
                     type="radio" 
                     name='plan'
                     value='basico'
                 /> Básico
-                <input 
+                <ImputRadio 
                     type="radio" 
                     name='plan'
                     value='completo'
                 /> Completo
-            </div>
+            </Campo>
 
-            <button type='button'>Cotizar</button>
+            <Boton type='button'>Cotizar</Boton>
         </form>      
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import styled from '@emotion/styled'
 
 const ContenedorHeader = styled.header`
-    background-color: #26C6DA;
+    background-color: #7CC644;
     padding: 10px;
     font-weight: bold;
     color: #fff;
+    margin-top: 50px;
 `;
 
 const TextoHeader = styled.h1`
