27563fc3112935eea5a6cafec0cd5fd9080f64e5
Zolouz966 <zrodrigo96@gmail.com>
2023-03-01 16:31:15 -0400

    Agregamos el componente formulario y emotion para usar el styled component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Header from "./components/Header";
+import styled from "@emotion/styled";
+import Formulario from "./components/Formulario";
+
+const Contenedor = styled.div `
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div `
+  background-color: #FFFFFF;
+  padding: 3rem;
+`
 
 function App() {
   return (
-    <Header 
-      Titulo='Cotizador de seguros'
-    />
+    <Contenedor>
+      <Header 
+        Titulo='Cotizador de seguros'
+      />
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
