95fb501cf524cd8d3fc6dd31994093b9e65cb4a1
Zolouz966 <zrodrigo96@gmail.com>
2023-03-01 17:10:23 -0400

    Validando el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from '@emotion/styled';
 
 const Campo = styled.div `
@@ -19,7 +19,7 @@ const Select = styled.select `
     -webkit-appearance: none;
 `;
 
-const ImputRadio = styled.input `
+const InputRadio = styled.input `
     margin: 0 1rem;
 
 `;
@@ -42,51 +42,147 @@ const Boton = styled.button `
     }
 `;
 
-const Formulario = () => {
-    return (
-        <form>
+const Error = styled.div `
+    background-color: #bc1823;
+    color: white;
+    padding: 1rem;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 2rem;
+`;
+
+const Formulario = ({guardarResumen, guardarCargando}) => {
+
+    const [ datos, guardarDatos ] = useState({
+        marca: '',
+        year: '',
+        plan: ''
+    });
+    const [ error, guardarError ] = useState(false);
+
+    // extraer los valores del state
+    const { marca, year, plan } = datos;
+
+    // Leer los datos del formulario y colocarlos en el state
+    const obtenerInformacion = e => {
+        guardarDatos({
+            ...datos,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    // cuando el usuario presiona submit
+    const cotizarSeguro = e => {
+        e.preventDefault();
+
+        if(marca.trim() === '' || year.trim() === '' || plan.trim() === '') {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+        // Una base de 2000
+        let resultado = 2000;
+
+        // obtener la diferencia de años
+        // const diferencia = obtenerDiferenciaYear(year);
+
+        // // por cada año hay que restar el 3%
+        // resultado -= (( diferencia * 3 ) * resultado) / 100;
+
+        // // Americano 15
+        // // Asiatico 5%
+        // // Europeo 30%
+        // resultado = calcularMarca(marca) * resultado;
+
+        // // Basíco aumenta 20%
+        // // Completo 50%
+        // const incrementoPlan = obtenerPlan(plan);
+        // resultado = parseFloat( incrementoPlan * resultado ).toFixed(2);
+
+        guardarCargando(true);
+
+        setTimeout(() => {
+
+            // Elimina el spinner
+            guardarCargando(false);
+
+            // pasa la información al componente principal
+            guardarResumen({
+                cotizacion: Number(resultado),
+                datos
+            });
+        }, 3000);
+
+
+
+    }
+
+    return ( 
+        <form
+            onSubmit={cotizarSeguro}
+        >
+            { error ? <Error>Todos los campos son obligatorios</Error>  : null }
+
             <Campo>
-                <Label for="">Marca</Label>
-                <Select>
-                    <option value="">Selecione</option>
-                        <option value="americado">Americano</option>
-                        <option value="europeo">Europeo</option>
-                        <option value="asiatico">Asiático</option>
-                </Select>
-            </Campo>
-            <Campo>
-                <Label for="">Marca</Label>
-                <Select>
+                <Label>Marca</Label>
+                <Select
+                    name="marca"
+                    value={marca}
+                    onChange={obtenerInformacion}
+                >
                     <option value="">-- Seleccione --</option>
-                        <option value="2021">2021</option>
-                        <option value="2020">2020</option>
-                        <option value="2019">2019</option>
-                        <option value="2018">2018</option>
-                        <option value="2017">2017</option>
-                        <option value="2016">2016</option>
-                        <option value="2015">2015</option>
-                        <option value="2014">2014</option>
-                        <option value="2013">2013</option>
-                        <option value="2012">2012</option>
+                    <option value="americano">Americano</option>
+                    <option value="europeo">Europeo</option>
+                    <option value="asiatico">Asiatico</option>
                 </Select>
             </Campo>
+
             <Campo>
-                <Label for="">Plan</Label>
-                <ImputRadio 
-                    type="radio" 
-                    name='plan'
-                    value='basico'
+                <Label>Año</Label>
+                <Select
+                    name="year"
+                    value={year}
+                    onChange={obtenerInformacion}
+                >
+                    <option value="">-- Seleccione --</option>
+                    <option value="2021">2021</option>
+                    <option value="2020">2020</option>
+                    <option value="2019">2019</option>
+                    <option value="2018">2018</option>
+                    <option value="2017">2017</option>
+                    <option value="2016">2016</option>
+                    <option value="2015">2015</option>
+                    <option value="2014">2014</option>
+                    <option value="2013">2013</option>
+                    <option value="2012">2012</option>
+                </Select>
+            </Campo>
+
+            <Campo>
+                <Label>Plan</Label>
+                <InputRadio 
+                    type="radio"
+                    name="plan"
+                    value="basico"
+                    checked={plan === "basico"}
+                    onChange={obtenerInformacion}
                 /> Básico
-                <ImputRadio 
-                    type="radio" 
-                    name='plan'
-                    value='completo'
+
+                <InputRadio 
+                    type="radio"
+                    name="plan"
+                    value="completo"
+                    checked={plan === "completo"}
+                    onChange={obtenerInformacion}
                 /> Completo
             </Campo>
 
-            <Boton type='button'>Cotizar</Boton>
-        </form>      
-    );
-};
+            <Boton type="submit">Cotizar</Boton>
+        </form>
+
+     );
+}
 
 export default Formulario;
